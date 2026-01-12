ca8ad36523bf48c4c24f1f078272c40a32d96ad4
helpingstar <iamhelpingstar@gmail.com>
2026-01-12 23:52:40 +0900

    chore: Bump version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "io.github.helpingstar.protest_alert"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
