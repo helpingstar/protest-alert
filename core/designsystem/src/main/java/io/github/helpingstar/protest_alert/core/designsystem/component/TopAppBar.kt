c6941c635dbc9e871ecacafbd228a6a1d2e6dc4f
helpingstar <iamhelpingstar@gmail.com>
2026-02-05 03:47:29 +0900

    feat(designsystem): add action icon to TopAppBar

--- core/designsystem/src/main/java/io/github/helpingstar/protest_alert/core/designsystem/component/TopAppBar.kt
+++ core/designsystem/src/main/java/io/github/helpingstar/protest_alert/core/designsystem/component/TopAppBar.kt
@@ -1,13 +1,17 @@
 package io.github.helpingstar.protest_alert.core.designsystem.component
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -16,11 +20,21 @@ fun PaTopAppbar(
     @StringRes titleRes: Int,
     modifier: Modifier = Modifier,
     colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(),
+    @DrawableRes actionIconRes: Int,
+    onActionClick: (() -> Unit),
 ) {
     CenterAlignedTopAppBar(
         title = { Text(text = stringResource(id = titleRes)) },
         colors = colors,
-        modifier = modifier
+        modifier = modifier,
+        actions = {
+            IconButton(onClick = onActionClick) {
+                Icon(
+                    painter = painterResource(id = actionIconRes),
+                    contentDescription = null
+                )
+            }
+        }
     )
 }
 
