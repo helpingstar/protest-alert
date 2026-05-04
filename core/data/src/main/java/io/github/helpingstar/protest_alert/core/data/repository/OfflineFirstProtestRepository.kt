f87d7a74c641a3454b141d946cb4c166ae7d1f16
helpingstar <iamhelpingstar@gmail.com>
2026-05-04 22:17:20 +0900

    feat: notify followed regions after sync

--- core/data/src/main/java/io/github/helpingstar/protest_alert/core/data/repository/OfflineFirstProtestRepository.kt
+++ core/data/src/main/java/io/github/helpingstar/protest_alert/core/data/repository/OfflineFirstProtestRepository.kt
@@ -9,28 +9,30 @@ import io.github.helpingstar.protest_alert.core.datastore.PaPreferencesDataSourc
 import io.github.helpingstar.protest_alert.core.model.data.ProtestResource
 import io.github.helpingstar.protest_alert.core.network.PaNetworkDataSource
 import io.github.helpingstar.protest_alert.core.network.model.NetworkProtestResource
+import io.github.helpingstar.protest_alert.core.notifications.Notifier
 import io.github.helpingstar.protest_alert.database.dao.ProtestResourceDao
 import io.github.helpingstar.protest_alert.database.dao.RegionDao
 import io.github.helpingstar.protest_alert.database.model.ProtestResourceEntity
 import io.github.helpingstar.protest_alert.database.model.asExternalModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.datetime.LocalDate
 import javax.inject.Inject
+import kotlin.time.Instant
 
-
+private val FIRST_SYNC_CUTOFF = Instant.parse("1970-01-01T00:00:00Z")
 private const val SYNC_BATCH_SIZE = 10
-
 
 internal class OfflineFirstProtestRepository @Inject constructor(
     private val paPreferencesDataSource: PaPreferencesDataSource,
     private val protestResourceDao: ProtestResourceDao,
     private val regionDao: RegionDao,
     private val network: PaNetworkDataSource,
-//    private val notifier: Notifier
+    private val notifier: Notifier,
 ) : ProtestRepository {
     override fun getProtestResources(
-        query: ProtestResourceQuery
+        query: ProtestResourceQuery,
     ): Flow<List<ProtestResource>> = protestResourceDao.getProtestResources(
         useFilterRegionIds = query.filterRegionIds != null,
         filterRegionIds = query.filterRegionIds ?: emptySet(),
@@ -40,10 +42,11 @@ internal class OfflineFirstProtestRepository @Inject constructor(
         .map { it.map(ProtestResourceEntity::asExternalModel) }
 
     override suspend fun syncWith(synchronizer: Synchronizer): Boolean {
-        // TODO(hs) have to handle it
+        var isFirstSync = false
         return synchronizer.changeListSync(
             versionReader = LastUpdatedAt::protestResourceLastUpdatedAt,
             changeListFetcher = { lastUpdatedAt ->
+                isFirstSync = lastUpdatedAt <= FIRST_SYNC_CUTOFF
                 network.getProtestResourceChangeList(after = lastUpdatedAt)
             },
             versionUpdater = { lastUpdatedAt ->
@@ -51,7 +54,26 @@ internal class OfflineFirstProtestRepository @Inject constructor(
             },
             modelDeleter = protestResourceDao::deleteProtestResources,
             modelUpdater = { changedIds ->
-                // TODO(hs) 온보딩, 알림기능 추가
+                val changedProtestResourceIds = changedIds
+                    .mapNotNull(String::toLongOrNull)
+                    .toSet()
+
+                val userData = paPreferencesDataSource.userData.first()
+                val hasOnboarded = userData.shouldHideOnboarding
+                val followedRegionIds = userData.followedRegions
+
+                val existingProtestResourceIdsThatHaveChanged = when {
+                    hasOnboarded -> protestResourceDao.getProtestResourceIds(
+                        useFilterRegionIds = true,
+                        filterRegionIds = followedRegionIds,
+                        useFilterProtestResourceIds = true,
+                        filterProtestResourceIds = changedProtestResourceIds
+                    )
+                        .first()
+                        .toSet()
+
+                    else -> emptySet()
+                }
 
                 changedIds.chunked(SYNC_BATCH_SIZE).forEach { chunkedIds ->
                     val networkProtestResources = network.getProtestResources(ids = chunkedIds)
@@ -64,8 +86,28 @@ internal class OfflineFirstProtestRepository @Inject constructor(
                     protestResourceDao.upsertProtestResources(
                         protestResourceEntities = networkProtestResources.map(
                             NetworkProtestResource::asEntity
-                        )
+                        ),
                     )
+                }
+
+                if (hasOnboarded && !isFirstSync) {
+                    val addedProtestResourceIds =
+                        changedProtestResourceIds - existingProtestResourceIdsThatHaveChanged
+
+                    val addedProtestResources = protestResourceDao.getProtestResources(
+                        useFilterRegionIds = true,
+                        filterRegionIds = followedRegionIds,
+                        useFilterProtestResourceIds = true,
+                        filterProtestResourceIds = addedProtestResourceIds,
+                    )
+                        .first()
+                        .map(ProtestResourceEntity::asExternalModel)
+
+                    if (addedProtestResources.isNotEmpty()) {
+                        notifier.postProtestNotifications(
+                            protestResources = addedProtestResources,
+                        )
+                    }
                 }
             }
 
